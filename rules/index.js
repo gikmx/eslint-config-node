9c932c1c85bde44c9e5b9248001385df3c4dc4be
Héctor Menéndez <hector@gik.mx>
2018-09-28 00:39:40 -0300

    Added root impott

--- rules/index.js
+++ rules/index.js
@@ -9,6 +9,17 @@ module.exports = {
         node: true,
     },
 
+    parserOptions: {
+        ecmaVersion: 2018,
+    },
+
+    'import/resolver': {
+        'babel-plugin-root-import': {
+            rootPathPrefix: '~',
+            rootPathSuffix: 'src',
+        }
+    }
+
     // Add rules specific to backend projects.
     rules: {// 0 = off, 1 = warn, 2 = error
     },
